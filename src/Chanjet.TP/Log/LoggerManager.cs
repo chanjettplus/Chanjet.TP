6ab7e8574a1bc851af4532496c33989a2923160f
chanjettp <zlj@chanjet.com>
2014-03-07 16:55:39 +0800

    commit

---
+++ src/Chanjet.TP/Log/LoggerManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chanjet.TP
+{
+  	public sealed class LoggerManager
+	{
+        public static ILogger GetLogger(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+		public static ILogger GetLogger(Type type)
+		{
+            throw new NotImplementedException();
+		}
+	}
+}
+
